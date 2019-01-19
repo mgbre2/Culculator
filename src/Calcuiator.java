3e36d0c2ca78f5f15907ed3c667e2f7c891918f7
mussie tewolde <mgbre2@yahoo.com>
2019-01-19 13:14:12 -0500

    edit again

--- src/Calcuiator.java
+++ src/Calcuiator.java
@@ -1,6 +1,6 @@
 public class Calcuiator {
-    public static double add (double nub1){
-return nub1;
+    public static double add (double nub1 , double num2){
+return nub1 -num2;
     }
     public static double substract(double num1,double num2){
         return num1-num2;
