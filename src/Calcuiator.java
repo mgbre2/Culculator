aece942c3b5eef9f457cadb8188d2aff60fceaff
mussie tewolde <mgbre2@yahoo.com>
2019-01-19 12:37:48 -0500

    add subtract method

--- src/Calcuiator.java
+++ src/Calcuiator.java
@@ -2,4 +2,7 @@ public class Calcuiator {
     public static double add (double nub1){
 return nub1;
     }
+    public static double substract(double num1,double num2){
+        return num1-num2;
+    }
 }
